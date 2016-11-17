0541eb62f40a3afde2c1abc60aff5aca76591701
mandm4s <trailcapital@gmail.com>
2016-11-16 23:26:26 -0500

    new file:   install-bin/install.cpp 	new file:   install-bin/install.h 	new file:   install-bin/install.o

---
+++ install-bin/install.h
@@ -0,0 +1,12 @@
+#ifndef INSTALL
+#define INSTALL
+class Install
+{
+	public:
+		void   mainfunc();
+		string createAction();
+		string createMenu();
+		string findActionsFolder();
+		string createOption(string, string);
+};
+#endif
